402865b5b7a24ab62b094353a28422d40d076686
lipne <lipnekontomerka@gmail.com>
2018-09-23 23:31:22 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zabawaZNullami
+{
+	class Program
+	{
+		
+			static void Main(string[] args)
+			{
+				bool? brakDanych = null;
+				bool? prawda = true;
+				bool? falsz = false;
+				// | - lub
+				// & - i
+
+				bool? wynik = brakDanych | prawda;
+
+				//		Console.WriteLine(wynik);
+				if (wynik == true)
+					Console.WriteLine("prawda");
+
+				if (wynik == false)
+					Console.WriteLine("fałsz");
+
+				if (wynik == null)
+					Console.WriteLine("nie wiem");
+
+				Console.ReadLine();
+			}
+	}
+}
